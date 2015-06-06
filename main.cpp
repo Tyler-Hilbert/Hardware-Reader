b83fdbbf0e8309556575dbe7cd84df2c5081d508
tyler-hilbert <tylerjoshuahilbert@gmail.com>
2015-06-06 15:21:28 -0400

    Added CPU and Ram parsing

--- main.cpp
+++ main.cpp
@@ -1,39 +1,74 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
-#include <sstream>
 
 using namespace std;
 
 struct attribute {
   string name;
   string value;
-  //unsigned long int value;
-  string unit;
 };
 
+struct hardware {
+  attribute ram;
+  attribute cpu;
+} hardware;
+
+void parseFile(string file);
+
 int main(int argc, const char** argv) {
+  parseFile("/proc/meminfo");
+  parseFile("/proc/cpuinfo");
+
+  cout 
+    << hardware.ram.name
+    << ": "
+    << hardware.ram.value 
+    << "\n"
+    << hardware.cpu.name 
+    << ": "
+    << hardware.cpu.value
+    << "\n";
+  
+  return(0);
+}
+
+void parseFile(string file) {
   // Get file to parse
   FILE *fp;
-  fp = fopen("/proc/meminfo","r");
+  fp = fopen(file.c_str(),"r");
   string read = "";
 
   // Parse file
-  while(!feof(fp)) {
-    char c = fgetc(fp);
+  char c;
+  do {
+    c = fgetc(fp);
+
+    if (feof(fp)) {
+      break;
+    }
+
     read += c;
     
     // If end of attribute, parse that attribute
     if (c == '\n') {
-      attribute atr;
-      istringstream stream(read);
-      stream >> atr.name >> atr.value >> atr.unit;
-      cout << atr.name << " " << atr.value << " " << atr.unit << "\n";
+      int colonPos;
+      if((colonPos = read.find(':') ) != -1) {
+	attribute atr;
+	atr.name = read.substr(0, colonPos);
+	read = read.substr(colonPos);
+	atr.value = read.substr(1, read.find('\n') - 1);
+
+	if (atr.name == "MemTotal") {
+	  hardware.ram = atr;
+	} else if (atr.name.substr(0, 10)  == "model name") {
+	  hardware.cpu = atr;
+	} 
+     }
+
       read = "";
     }
-  }
+  } while (true);
 
-  fclose(fp); 
-  
-  return(0);
+  fclose(fp);
 }
