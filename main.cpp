276c8c066ba23cc07eb442ca6b461fa4e6f1a7fd
tyler-hilbert <tylerjoshuahilbert@gmail.com>
2015-06-08 22:59:15 -0400

    Added parsing all hardware

--- main.cpp
+++ main.cpp
@@ -1,38 +1,69 @@
 #include <stdio.h>
 #include <string>
-#include <iostream>
 #include <algorithm>
 #include <functional>
 #include <cctype>
 #include <locale>
 #include <unordered_map>
+#include <iostream>
+#include <sstream>
 
 using namespace std;
 
 
-void parseFile(unordered_map<string,string> &attributeList, string file);
+void parseProcFile(unordered_map<string,string> &attributeList, string file);
+void parseDmi(unordered_map<string,string> &attributeList, string cmd);
+void parseDmiRamSlots(unordered_map<string,string> &attributeList);
+void parseDmiGPU(unordered_map<string,string> &attributeList);
 string trim(string s);
+
+string const MANUFACTURER = "Manufacturer";
+string const CPU_NAME = "Computer Name";
+string const RAM = "RAM";
+string const CPU = "CPU";
+string const MAX_RAM = "RAM Maximum Capacity";
+string const RAM_SLOT = "Ram Slot ";
+int ramSlots = 0; // The number of ram slots that are on the computer
+string const GPU = "Graphics Card ";
+int numGPU = 0; // The number of graphics card in the computer
 
 
 int main(int argc, const char** argv) {
   unordered_map<string,string> attributeList;
-  parseFile(attributeList, "/proc/meminfo");
-  parseFile(attributeList, "/proc/cpuinfo");
+  parseProcFile(attributeList, "/proc/meminfo");
+  parseProcFile(attributeList, "/proc/cpuinfo");
+  parseDmi(attributeList, "sudo dmidecode -t system");
+  parseDmi(attributeList, "sudo dmidecode -t 16");
+  parseDmiRamSlots(attributeList);
+  parseDmiGPU(attributeList);
 
-  cout
-    << "RAM: "
-    << attributeList["RAM"]
-    << "\n"
-    << "CPU: "
-    << attributeList["CPU"]
-    << "\n";
+  cout 
+    << MANUFACTURER << ": " << attributeList[MANUFACTURER] << "\n"
+    << CPU_NAME << ": " << attributeList[CPU_NAME] << "\n"
+    << CPU << ": " << attributeList[CPU] << "\n"
+    << RAM << ": " << attributeList[RAM] << "\n"
+    << MAX_RAM << ": " << attributeList[MAX_RAM] << "\n";
+
+  for (int i = 0; i < ramSlots; i++) {
+    ostringstream ram;
+    ram << RAM_SLOT << i;
+    cout << ram.str() <<  ": " << attributeList[ram.str()] << "\n";
+  }
+
+  for (int i = 0; i < numGPU; i++) {
+    ostringstream gpu;
+    gpu << GPU << i;
+    cout << gpu.str() <<  ": " << attributeList[gpu.str()] << "\n";
+  }
+
   return(0);
 }
 
 /**
-   Parses proc info file
+   Parses proc info file.
+   Adds ram and cpu to attributeList
 */
-void parseFile(unordered_map<string,string> &attributeList, string file) {
+void parseProcFile(unordered_map<string,string> &attributeList, string file) {
   // Get file to parse
   FILE *fp;
   fp = fopen(file.c_str(),"r");
@@ -58,10 +89,150 @@ void parseFile(unordered_map<string,string> &attributeList, string file) {
 	string value = trim(read.substr(1, read.find('\n') - 1));
 
 	if (name == "MemTotal") {
-	  attributeList.emplace("RAM", value); 
+	  attributeList.emplace(RAM, value); 
 	 
 	} else if (name == "model name") {
-	  attributeList.emplace("CPU", value);
+	  attributeList.emplace(CPU, value);
+	} 
+     }
+
+      read = "";
+    }
+  } while (true);
+
+  fclose(fp);
+}
+
+
+/** 
+    Parse dmidecode cmd to get manufacturer, product name, or maximum ram capacity
+*/
+void parseDmi(unordered_map<string,string> &attributeList, string cmd) {
+  // Get file to parse
+  FILE *fp;
+  fp = popen(cmd.c_str(), "r");
+  string read = "";
+
+  // Parse file
+  char c;
+  do {
+    c = fgetc(fp);
+
+    if (feof(fp)) {
+      break;
+    }
+
+    read += c;
+    
+    // If end of attribute, parse that attribute
+    if (c == '\n') {
+      int colonPos;
+      if((colonPos = read.find(':') ) != -1) {
+	string name = trim(read.substr(0, colonPos));
+	read = read.substr(colonPos);
+	string value = trim(read.substr(1, read.find('\n') - 1));
+
+	if (name == "Manufacturer") {
+	  attributeList.emplace(MANUFACTURER, value); 
+	} else if (name == "Product Name") {
+	  attributeList.emplace(CPU_NAME, value);
+	  break;
+	} else if (name == "Maximum Capacity") {
+	  attributeList.emplace(MAX_RAM, value);
+	  break;
+	}
+     }
+
+      read = "";
+    }
+  } while (true);
+
+  fclose(fp);
+}
+
+/**
+   Parses dmidecode -t 17 to get the ram slots
+*/
+void parseDmiRamSlots(unordered_map<string,string> &attributeList) {
+  // Get file to parse
+  FILE *fp;
+  fp = popen("sudo dmidecode -t 17", "r");
+  string read = "";
+
+  // Parse file
+  char c;
+  do {
+    c = fgetc(fp);
+
+    if (feof(fp)) {
+      break;
+    }
+
+    read += c;
+    
+    // If end of attribute, parse that attribute
+    if (c == '\n') {
+      int colonPos;
+      if((colonPos = read.find(':') ) != -1) {
+	string name = trim(read.substr(0, colonPos));
+	read = read.substr(colonPos);
+	string value = trim(read.substr(1, read.find('\n') - 1));
+
+	if (name == "Size") {
+	  ostringstream ram;
+	  ram << RAM_SLOT << ramSlots;
+	  attributeList.emplace(ram.str(), value);
+	  ramSlots++;
+	} 
+     }
+
+      read = "";
+    }
+  } while (true);
+
+  fclose(fp);
+}
+
+/**
+   Parses dmidecode -t baseboard to get the GPU info
+*/
+void parseDmiGPU(unordered_map<string,string> &attributeList) {
+  // Get file to parse
+  FILE *fp;
+  fp = popen("sudo dmidecode -t baseboard", "r");
+  string read = "";
+
+  // Parse file
+  char c;
+  bool parsingGPU = false; // Whether the device being parsed in currently a video card
+  do {
+    c = fgetc(fp);
+
+    if (feof(fp)) {
+      break;
+    }
+
+    read += c;
+    
+    // If end of attribute, parse that attribute
+    if (c == '\n') {
+      int colonPos;
+      if((colonPos = read.find(':') ) != -1) {
+	string name = trim(read.substr(0, colonPos));
+	read = read.substr(colonPos);
+	string value = trim(read.substr(1, read.find('\n') - 1));
+	
+	// Check if the GPU has started being parsed
+	if (name == "Type" && value == "Video") {
+	  parsingGPU = true;
+	}
+
+	if (parsingGPU && name == "Description") {
+	  ostringstream gpu;
+	  gpu << GPU << numGPU;
+	  attributeList.emplace(gpu.str(), value);
+	  numGPU++;
+	  parsingGPU = false;
 	} 
      }
 
